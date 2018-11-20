b9d42f4ba8aac61d9b2d93557dc2195a7dfbe207
DevMMing <mming@stuy.edu>
2018-11-20 15:36:53 -0500

    added changes to shell.c

--- shell.c
+++ shell.c
@@ -2,26 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <time.h>
 #include "shell.h"
 
-int main(){
-  char line[] = "ls -l -a";
-  char ** args = parse_args(line);
-  execvp(args[0], args);
-  return 0;
+int main(int argc, char *argv[]){
+	char *line;
+	if(argc > 1){
+		line = argv[1];
+	}else{
+		printf("enter command\n");
+		scanf("%s",&line);
+	}
+	char ** args = parse_args(line);
+	execvp(args[0], args);
+	return 0;
 }
 
 char ** parse_args( char * line ){
-  int count = 1;
-  char * temp = strchr(line,' ');
-  while(temp != 0){
-    count++;
-    temp = strchr(temp + 1,' ');
-  }
-  char **args = (char**)malloc(count * sizeof(char*));
-  for(int i = 0;i < count + 1;i++){
-    args[i] = strsep(&line, " ");
-  }
-  return args;
+	int count = 1;
+	char * temp = strchr(line,' ');
+	while(temp != 0){
+		count++;
+		temp = strchr(temp + 1,' ');
+	}
+	char **args = (char**)malloc(count * sizeof(char*));
+	for(int i = 0;i < count + 1;i++){
+		args[i] = strsep(&line, " ");
+	}
+	return args;
 }
 
