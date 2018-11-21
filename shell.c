906e09043319c1e55e49e4876da10afd3e7c8145
Andy Zhou <azhou4@stuy.edu>
2018-11-21 15:36:31 -0500

    fixed most of the running

--- shell.c
+++ shell.c
@@ -11,29 +11,35 @@
 #include "shell.h"
 
 int main(int argc, char *argv[]){
-	char *line;
-	if(argc > 1){
-		line = argv[1];
-	}else{
-		printf("enter command\n");
-		scanf("%s",&line);
-	}
-	char ** args = parse_args(line);
-	execvp(args[0], args);
-	return 0;
+  if(argc > 1){
+    char * args[argc];
+    for(int i = 0;i < argc - 1;i++){
+      args[i] = argv[i+1];
+    }
+    args[argc - 1] = '\0';
+    execvp(args[0], args);
+  }else{
+    char ** args;
+    char *line;
+    printf("enter command\n");
+    scanf("%99[^\n]",line);
+    args = parse_args(line);
+    execvp(args[0], args);
+  }
+  return 0;
 }
 
 char ** parse_args( char * line ){
-	int count = 1;
-	char * temp = strchr(line,' ');
-	while(temp != 0){
-		count++;
-		temp = strchr(temp + 1,' ');
-	}
-	char **args = (char**)malloc(count * sizeof(char*));
-	for(int i = 0;i < count + 1;i++){
-		args[i] = strsep(&line, " ");
-	}
-	return args;
+  int count = 1;
+  char * temp = strchr(line,' ');
+  while(temp != 0){
+    count++;
+    temp = strchr(temp + 1,' ');
+  }
+  char **args = (char**)malloc(count * sizeof(char*));
+  for(int i = 0;i < count + 1;i++){
+    args[i] = strsep(&line, " ");
+  }
+  return args;
 }
 
