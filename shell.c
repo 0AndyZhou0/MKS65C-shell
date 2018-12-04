7f11b48e7007bcbe4804ec03a3f35582890070c0
0AndyZhou0 <azhou4@stuy.edu>
2018-12-03 20:15:37 -0500

    pipes working I think , I'm too dead to test it

--- shell.c
+++ shell.c
@@ -43,39 +43,60 @@ char ** run_semicolon(char * line){
   while(line){
     temp = strsep(&line, ";");
     if(temp){
-		if(strchr(temp,'|')){
-			run_pipe(temp);
-		}
-		else{
-			run_command(temp);
-		}
+      if(strchr(temp,'|')){
+	run_pipe(temp);
+      }
+      else{
+	run_command(temp);
+      }
     }
   }
 }
 
 char ** run_pipe(char * line){
   char * temp;
-  char in[100]=" ";
+  char backup[100];
+  char in[100];
   int start = 0;
-  while(line){
+  while(line && strchr(line,'|')){
     temp = strsep(&line, "|");
+    strcpy(backup,line); // backsup line because strcat overwrites line
     if(temp){
-		if(start == 0){
-			strcat(temp," > something");
-			run_command(temp);
-			start++;
-		}
-		else{
-			strcpy(in,temp);
-			strcat(in," < something");
-			run_command(in);
-			strcat(temp," > something");
-			run_command(temp);
-		}
+      if(start == 0){
+	strcat(temp," > something");
+	run_command(temp);
+	start++;
+      }else{
+	int backupfile = dup(STDOUT_FILENO);	
+	int file = open("something2", O_CREAT | O_RDWR, 0777);
+	dup2(file,1);
+	strcat(temp," < something");
+	run_command(temp);
+	dup2(backupfile, 1);
+	close(file);
+
+	//Moving contents of file
+	FILE * file1 = fopen("something2", "r");
+	FILE * file2 = fopen("something", "w");
+	int c;
+	while((c = fgetc(file1)) != EOF){
+	  fputc(c, file2);
+	}
+	fclose(file1);
+	fclose(file2);
+      }
+      strcpy(line,backup);
     }
-	
   }
+  if(line){
+    //printf("%s\n",line);
+    strcat(line," < something");
+    run_command(line);
+  }
+  //char * args[] = {"rm", "rm something"};
+  //execvp(args[0], args);
 }
+
 char * white_out(char * string){
   char * no_space = string;
   int i = 0;
@@ -100,38 +121,40 @@ void run_command(char * command){
   //Removed counting
   char **args = (char**)malloc(99 * sizeof(char*));
   int i = 0;
-  int leftArr = 0;
-  int rightArr =0;
   
   while(command){
     args[i] = strsep(&command, " ");
     if(args[i]){
       strcpy(args[i],white_out(args[i]));
-	}
+    }
     if(args[i][0] == '\0'){
       i--;
     }
     i++;
   }
   args[i] = 0;
+  
   //Hard Coded Commands
   if (!strcmp(args[0], "exit")) {exit(0);}  
   else if(!strcmp(args[0], "cd")){
     chdir(args[1]);
   }
+  
   char output[100];
   char input[100];
-  for(int i =0;args[i]!='\0';i++){ //goes through args finding any arrows     
-			if(!strcmp(args[i],">")){
-			args[i]=NULL;
-            strcpy(output,args[i+1]);
-			rightArr=i;
-			}
-			else if(!strcmp(args[i],"<")){
-			args[i]=NULL;
-			strcpy(input,args[i+1]);
-			leftArr=i;
-			}   
+  int leftArr = 0;
+  int rightArr =0;
+  for(int i =0;args[i];i++){ //goes through args finding any arrows     
+    if(!strcmp(args[i],">")){
+      args[i]=NULL;
+      strcpy(output,args[i+1]);
+      rightArr=i;
+    }
+    else if(!strcmp(args[i],"<")){
+      args[i]=NULL;
+      strcpy(input,args[i+1]);
+      leftArr=i;
+    }   
   }
   //Running process
   child = fork();
@@ -140,27 +163,27 @@ void run_command(char * command){
     pid = wait(&status);
   }else{
     //Child Code
-	if(leftArr){//runs if using left arrow
-		int fd0=open(input,O_RDONLY);
-		dup2(fd0, STDIN_FILENO);
-		close(fd0); 
-	}
-	if(rightArr){//runs if using right arrow
-		int fd1=open(output,O_CREAT|O_WRONLY, 0777);
-		dup2(fd1, STDOUT_FILENO);
-		close(fd1);
+    if(leftArr){//runs if using left arrow
+      int fd0=open(input,O_RDONLY);
+      dup2(fd0, STDIN_FILENO);
+      close(fd0); 
     }
-	execvp(args[0], args);
-	free(args);
-	}
+    if(rightArr){//runs if using right arrow
+      int fd1=open(output,O_CREAT|O_WRONLY, 0777);
+      dup2(fd1, STDOUT_FILENO);
+      close(fd1);
+    }
+    execvp(args[0], args);
+  }
+  free(args);
 }
 /*
   Old Counting Code
   int count = 1;
   char * temp = strchr(command,' ');
   while(temp != 0){
-    temp = strchr(temp + 1,' ');
-    count++;
+  temp = strchr(temp + 1,' ');
+  count++;
   }
 
- */
+*/
